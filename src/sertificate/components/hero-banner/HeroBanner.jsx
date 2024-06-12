7c8dc1877bf872edee0e2b7161a8098455467f3d
azimjon-95 <>
2024-06-12 23:00:56 +0500

    first com

--- src/sertificate/components/hero-banner/HeroBanner.jsx
+++ src/sertificate/components/hero-banner/HeroBanner.jsx
@@ -1,39 +1,31 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./HeroBanner.css";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiDownload } from "react-icons/fi";
 import { Link, Outlet } from "react-router-dom";
-import axios from '../../api/api'
+import axios from '../../api/api';
 import hero_banner from "../../assets/main/bg.png";
 import Header from "../header/Header";
 import ReactToPrint from 'react-to-print';
-import { FiDownload } from 'react-icons/fi';
-import { BsFillTrashFill } from 'react-icons/bs';
 import Cert from '../certificates/Sert/Cert';
 import DipCertificat from '../certificates/Dip/Dip';
 
 const HeroBanner = () => {
   const componentRef = useRef();
   const [data, setData] = useState([]);
-  const [value, setValue] = useState('');
-  const [idD, setIdD] = useState("S0001");
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedID, setSelectedID] = useState("S0001");
 
-  const checkID = (id) => {
-    setIdD(id);
-  };
   useEffect(() => {
-    if (value.length) {
+    if (searchValue.length) {
       axios
         .get(`/certificate/all`)
-        .then(response => setData(response?.data))
-        .catch((err) => console.log(err));
-      return;
-    } else {
-      return;
+        .then(response => setData(response.data))
+        .catch(err => console.log(err));
     }
-  }, [value]);
-  let searchedData = data.filter(i => i?.id === value);
-  sessionStorage.setItem("data", JSON.stringify(searchedData))
+  }, [searchValue]);
 
+  const searchedData = data.filter(item => item?.id === searchValue);
+  sessionStorage.setItem("data", JSON.stringify(searchedData));
 
   const TableRow = ({ obj }) => {
     const {
@@ -49,61 +41,54 @@ const HeroBanner = () => {
 
     return (
       <table border={1} className="SearchPDF" key={_id}>
-        <tbody>
+        <thead>
           <tr>
             <th>ID</th>
             <th>Familiya Ism Otasining ismi</th>
             <th>Yuklab olish</th>
           </tr>
-        </tbody>
+        </thead>
         <tbody>
           <tr>
             <td>{id}</td>
-            <td>{lastname} {firstname} {other}</td>
+            <td>{`${lastname} ${firstname} ${other}`}</td>
             <td>
               <ReactToPrint
                 trigger={() => (
-                  <button
-                    onFocus={() => checkID(_id)}
-                    className="driverTab"
-                  >
+                  <button onFocus={() => setSelectedID(_id)} className="driverTab">
                     <FiDownload /><span>PDF</span>
                   </button>
                 )}
                 content={() => componentRef.current}
               />
             </td>
-
-            <td style={{ display: "none" }}>
-              {courseName === "cert" && _id === idD ? (
-                <Cert
-                  ref={componentRef}
-                  obj={{ idD: _id, id, prosent, firstname, lastname, courseName, other: other, givenDate }}
-                />
-              ) : courseName === "dip" && _id === idD ? (
-                <DipCertificat
-                  ref={componentRef}
-                  obj={{ idD: idD, id, prosent, firstname, lastname, courseName, other: other, givenDate }}
-                />
-              ) : null}
-            </td>
           </tr>
         </tbody>
+        <tfoot style={{ display: "none" }}>
+          {courseName === "cert" && _id === selectedID ? (
+            <Cert
+              ref={componentRef}
+              obj={{ idD: _id, id, prosent, firstname, lastname, courseName, other, givenDate }}
+            />
+          ) : courseName === "dip" && _id === selectedID ? (
+            <DipCertificat
+              ref={componentRef}
+              obj={{ idD: _id, id, prosent, firstname, lastname, courseName, other, givenDate }}
+            />
+          ) : null}
+        </tfoot>
       </table>
     );
   };
+
   return (
     <div className="bannerHero">
       <Header />
-
       <div className="heroBanner">
         <div className="heroBanner-left">
           <h1 className="heroBanner-title">Yagona Buxgalteriya rasmiy sertifikatlari</h1>
           <div className="imgBanerMobile">
-            <img
-              src={hero_banner}
-              alt="heroBanner-images"
-            />
+            <img src={hero_banner} alt="heroBanner-images" />
           </div>
           <div className="checker_wrapper">
             <input
@@ -113,20 +98,16 @@ const HeroBanner = () => {
               autoFocus
               placeholder="ID raqamingizni kiriting"
               className="check_certificate-input"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-            // onBlur={() => setSearch(true)}
-
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
             />
             <FiSearch />
           </div>
-          {value ?
+          {searchValue ? (
             <div className="studentResult">
               {searchedData.length ? (
-                searchedData.map(i => (
-                  <div key={i}>
-                    <TableRow key={i._id} obj={i} />
-                  </div>
+                searchedData.map(item => (
+                  <TableRow key={item._id} obj={item} />
                 ))
               ) : (
                 <div className="Loadin">
@@ -134,31 +115,20 @@ const HeroBanner = () => {
                 </div>
               )}
             </div>
-            :
+          ) : (
             <div className="imgBox">
-              <img src="https://media2.giphy.com/media/IzWnWcZHgGVaR3vglp/giphy.gif?cid=ecf05e47nya1o7uxhopxiaahv84cb3dphxwifktoy8ut9tci&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="" />
+              <img src="https://media2.giphy.com/media/IzWnWcZHgGVaR3vglp/giphy.gif?cid=ecf05e47nya1o7uxhopxiaahv84cb3dphxwifktoy8ut9tci&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="placeholder" />
             </div>
-          }
+          )}
         </div>
-
         <div className="imgBaner">
-          <img
-            src={hero_banner}
-            alt="heroBanner-images"
-          />
+          <img src={hero_banner} alt="heroBanner-images" />
         </div>
         <div className="bottomBG"></div>
-      </div >
-
+      </div>
       <Outlet />
     </div>
-
   );
 };
+
 export default HeroBanner;
-
-
-
-
-
-
