d0874d661d204b485f82bcddab79e65b435a47e4
bekicom <adelkauz6@gmail.com>
2024-06-28 21:01:29 +0500

    dsds

--- src/sertificate/components/certificates/Dip/Dip.js
+++ src/sertificate/components/certificates/Dip/Dip.js
@@ -3,6 +3,8 @@ import './style.css'
 import QRCode from "react-qr-code";
 import { AuthContext } from "../../../context/AuthContext";
 
+import bgfoto  from  "./DIPLOM22129.png"
+
 const DipCertificat = React.forwardRef((props, ref) => {
   const { URL } = useContext(AuthContext);
 
@@ -17,6 +19,8 @@ const DipCertificat = React.forwardRef((props, ref) => {
   } = props.obj;
   return (
     <div className="certificat_ContainerEng" ref={ref}>
+      <img  id="imgdiplom"  src={bgfoto} alt="" />
+    
       <div className="containerDip">
 
         <h3 className="engID4">
